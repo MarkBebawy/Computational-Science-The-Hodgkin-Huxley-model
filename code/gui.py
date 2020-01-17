14676b002aa8d7cb29fd45875874f63c3a09bb93
Mark Bebawy <markbebawy0@gmail.com>
2020-01-17 14:51:06 +0100

    Added entry widgets to GUI. WIP: make it functional

--- code/gui.py
+++ code/gui.py
@@ -7,37 +7,48 @@ import tkinter as tk
 
 def setup_start():
     """This function makes a screen, adds all labels, entry
-    widgets and text buttons and shows the window to the user."""
-    screen = tk.Tk(screenName="Hodgkin-Huxley model implementation")
+    widgets and text buttons and returns the screen and the entry
+    widgets."""
+    screen = tk.Tk()
     welcome_str = ("Welcome to the Hodgkin-Huxley GUI.\nOption 1: One action potential"
         "can be simulated and plotted.\nOption 2: Temperature experiments"
-        "can be run.")
+        "can be run.\n\nGeneral options")
     tk.Label(screen, text=welcome_str).grid(row=0)
 
-    # Options for current injection.
+    ## Options for current injection.
     tk.Label(screen, text="Amount of injected current").grid(row=1)
     tk.Label(screen, text="Start time for current injection").grid(row=2)
     tk.Label(screen, text="End time for current injection").grid(row=3)
 
-    # Options for numerical method.
+    ## Options for numerical method.
     tk.Label(screen, text="Numerical method (RK4=0, Forw. Euler=1)").grid(row=4)
     tk.Label(screen, text="Size of time steps for numerical method").grid(row=5)
 
-    # Options specific for one action potential simulation.
-    tk.Label(screen, text="Option 1 variables (option 2 will be ignored)").grid(row=6)
+    ## Options specific for one action potential simulation.
+    tk.Label(screen, text="\nOption 1 variables (option 2 will be ignored)").grid(row=6)
     tk.Label(screen, text="Temperature (degrees celsius)").grid(row=7)
     tk.Label(screen, text="Run time (miliseconds)").grid(row=8)
 
-    # Options specific for temperature experiments.
-    tk.Label(screen, text="Option 2 variables (option 1 will be ignored)").grid(row=9)
+    ## Options specific for temperature experiments.
+    tk.Label(screen, text="\nOption 2 variables (option 1 will be ignored)").grid(row=9)
     tk.Label(screen, text="Minimum temperature (degrees celsius)").grid(row=10)
     tk.Label(screen, text="Maximum temperature (degrees celsius)").grid(row=11)
     tk.Label(screen, text="Amount of experiments points in temperature range").grid(row=12)
     tk.Label(screen, text="Tolerance for resting potential").grid(row=13)
 
-    # Make entry widget for every variable.
-    num_vars = 12
+    # XXX: Change this if variables are changed.
+    num_rows = 14
+    title_rows = [0, 6, 9]
+    variable_rows = [i for i in range(num_rows) if i not in title_rows]
+
+    # Make a list of entry widgets.
     entry_widgets = list()
 
-    for i in range(num_vars):
-        pass
+    for indx, row in enumerate(variable_rows):
+        entry_widgets.append(tk.Entry(screen))
+        entry_widgets[indx].grid(row=row, column=1)
+
+    return screen, entry_widgets
+
+screen, entry_widgets = setup_start()
+screen.mainloop()
