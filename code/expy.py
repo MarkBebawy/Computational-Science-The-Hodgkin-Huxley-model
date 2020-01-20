0562cb56686e6228b30acbfa9464630cb5f7fea3
Mark Bebawy <markbebawy0@gmail.com>
2020-01-20 12:32:06 +0100

    Changed gitignore

--- code/expy.py
+++ code/expy.py
@@ -3,43 +3,6 @@ import numpy as np
 import hh
 import csv
 
-# def speedTemperature(x=hh.HodgkinHuxley()):
-#     """This function runs the Hodgkin-Huxley model for different temperatures
-#     and measures the time it takes to finish a single action potential.
-
-#     Parameters:
-#     - eps: error marigin around -65 mV such that [-65 - eps, -65 + eps]
-#            is accepted as resting potential.
-#     """
-#     temperatures = np.linspace(x.min_temp, x.max_temp, x.amount_temp_range)
-#     print(f"Running action potential for temperatures: {temperatures}")
-#     ap_times = list()
-#     for T in temperatures:
-#         print(f"Running {T} degrees...")
-#         x.set_temperature(T)
-#         eps = x.rest_potential_eps
-#         t, y = x.solve_model()
-#         volts = y[:,0]
-#         for index, v in enumerate(volts):
-#             if -65 - eps < v and v < -65 + eps and t[index] > 4:
-#                 ap_times.append(t[index])
-#                 break
-#             if index == len(volts) - 1:
-#                 print(f"WARNING: Did not find repolarisation for {T} degrees")
-#     return temperatures, ap_times
-
-
-# def plot(x, y):
-#     """Plot y against x."""
-#     plt.plot(x, y)
-#     plt.show()
-
-
-# if __name__ == "__main__":
-#     temperatures, ap_times = speedTemperature()
-#     plt.plot(temperatures, ap_times)
-#     plt.show()
-
 class CurrentParamters:
     def __init__(self, Imean = 20, Ivar = 1, Tmean = 1, Tvar = 0.1):
         """Store current paramters in object.
@@ -47,7 +10,7 @@ class CurrentParamters:
         - Imean: mean current strength,
         - Ivar: variance of current strength,
         - Tmean: mean time,
-        - Tvar: mean variance
+        - Tvar: time variance
         """
         self.Imean = Imean
         self.Ivar = Ivar
@@ -56,7 +19,6 @@ class CurrentParamters:
 
     def genCurrent(self):
         """Return a current function normaly with distributed time and strength"""
-        #TODO: could give negative duration/strength
         strength = np.random.normal(self.Imean, self.Ivar)
         duration = np.random.normal(self.Tmean, self.Tvar)
         strength = max(strength, 0)
@@ -64,7 +26,6 @@ class CurrentParamters:
         def I(t):
             return strength*(t < duration)
         return I
-
 
 
 class TempExperiment:
@@ -90,6 +51,7 @@ class TempExperiment:
         - eps: error marigin around -65 mV such that [-65 - eps, -65 + eps]
         is accepted as resting potential.
         """
+        # TODO: documenteren, docstring aanpassen.
         temperatures = np.linspace(self.minTemp, self.maxTemp, self.tempSteps)
         ap_durations = []
         for T in temperatures:
@@ -103,7 +65,7 @@ class TempExperiment:
         self.results = (temperatures, ap_durations)
 
     def genCurrent(self):
-        """return current with stored paramters"""
+        """Return current with stored paramters"""
         return self.currentPar.genCurrent()
 
     def determineDuration(self, t, volts, rest_pot):
@@ -157,28 +119,3 @@ TE = TempExperiment(quick=True)
 TE.run()
 TE.plot()
 plt.show()
-
-
-
-
-
-# def speedTemperature(minTemp=6.3, maxTemp=46.3, tempSteps=10, rkTimeSteps=0.0001, runTime=10, eps=10):
-#     """This function runs the Hodgkin-Huxley model for different temperatures
-#     and measures the time it takes to finish a single action potential.
-
-#     Parameters:
-#     - eps: error marigin around -65 mV such that [-65 - eps, -65 + eps]
-#            is accepted as resting potential.
-#     """
-#     temperatures = np.linspace(minTemp, maxTemp, tempSteps)
-#     ap_times = list()
-#     for T in temperatures:
-#         model = hh.HodgkinHuxley(T)
-#         t, y = model.solve_model(rkTimeSteps, runTime)
-#         volts = y[:,0]
-#         for index, v in enumerate(volts):
-#             if -65 - eps < v and v < -65 + eps and t[index] > 4:
-#                 ap_times.append(t[index])
-#                 break
-#     return temperatures, ap_times
-
