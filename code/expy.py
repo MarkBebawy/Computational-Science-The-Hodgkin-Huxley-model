6fdb5c7957750f0a96928cb2a6a01abf66bab983
Jim Wagemans <jimwagemans@gmail.com>
2020-01-20 12:27:26 +0100

    Changed current function

--- code/expy.py
+++ code/expy.py
@@ -41,18 +41,19 @@ import csv
 #     plt.show()
 
 class CurrentParamters:
-    def __init__(self, Imean = 20, Ivar = 1, Tmean = 1, Tvar = 0.1):
+    def __init__(self, Imean = 20, Ivar = 1, Tmean = 1, Tvar = 0.1, start_time=0):
         """Store current paramters in object.
         Parameters:
         - Imean: mean current strength,
         - Ivar: variance of current strength,
         - Tmean: mean time,
-        - Tvar: mean variance
+        - Tvar: time variance
         """
         self.Imean = Imean
         self.Ivar = Ivar
         self.Tmean = Tmean
         self.Tvar = Tvar
+        self.start_time = start_time
 
     def genCurrent(self):
         """Return a current function normaly with distributed time and strength"""
@@ -62,19 +63,18 @@ class CurrentParamters:
         strength = max(strength, 0)
         duration = max(duration, 0)
         def I(t):
-            return strength*(t < duration)
+            return strength*( self.start < t < self.start + duration)
         return I
 
 
 
 class TempExperiment:
-    def __init__(self, minTemp=6.3, maxTemp=46.3, tempSteps=10, timeStep=0.0001, runTime=10, tol=10, quick=False, currentPar = None):
+    def __init__(self, minTemp=6.3, maxTemp=46.3, tempSteps=10, model=hh.HodgkinHuxley(), tol=10, currentPar=None):
         """Initialize values used experiment"""
         self.minTemp = minTemp
         self.maxTemp = maxTemp
         self.tempSteps = tempSteps
-        self.timeStep = timeStep
-        self.runTime = runTime
+        self.model = model
         self.tol = tol
         self.quick = quick
         if currentPar is None:
@@ -91,12 +91,15 @@ class TempExperiment:
         is accepted as resting potential.
         """
         temperatures = np.linspace(self.minTemp, self.maxTemp, self.tempSteps)
+        print(f"Running action potential for temperatures: {temperatures}")
         ap_durations = []
+        model = self.model
+        rest_pot = model.V_eq
         for T in temperatures:
-            model = hh.HodgkinHuxley(T=T)
-            rest_pot = model.V_eq
+            print(f"Running {T} degrees...")
+            model.set_temperature(T)
             model.I = self.genCurrent()
-            t, y = model.solve_model(self.timeStep, self.runTime, quick=self.quick)
+            t, y = model.solve_model()
             volts = y[:,0]
             duration = self.determineDuration(t, volts, rest_pot)
             ap_durations.append(duration)
@@ -131,7 +134,7 @@ class TempExperiment:
         plt.title(title)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.figtext(0.99, 0.5, 'footnote text \n test \n test', horizontalalignment='right')
+        #plt.figtext(0.99, 0.5, 'footnote text \n test \n test', horizontalalignment='right')
         plt.show()
 
     def store_csv(self, file_name):
